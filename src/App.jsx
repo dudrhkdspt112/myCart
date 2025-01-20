e96b11e7bd1499229e5595571a20eb613d9bf0ed
dudrhkdspt112 <dudrhkdspt112@gmail.com>
2025-01-20 11:42:01 +0900

    상품들페이지

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css'
 import HomePage from './components/Home/HomePage'
 import Navbar from './components/Navbar/Navbar'
+import ProductsPage from './components/Products/ProductsPage'
 
 function App() {
  
   return (
     <div className='app'>
 		<Navbar/>
-		<main>
-			<HomePage />
-		</main>
+        <main>
+            {/* <HomePage/> */}
+            <ProductsPage/>
+        </main>
 	</div>
   )
 }
--- src/components/Products/ProductsPage.jsx
+++ src/components/Products/ProductsPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './ProductsPage.css';
+import ProductsSidebar from './ProductsSidebar';
+import ProductsList from './ProductsList';
 
 const ProductsPage = () => {
   return (
-    <div>ProductsPage</div>
+    <section className='products_page'>
+		<ProductsSidebar/>
+		<ProductsList></ProductsList>
+	</section>
   )
 }
 
