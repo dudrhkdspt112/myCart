b7ca760a0a51aa66fba27b4a2f4ebf36f9715615
dudrhkdspt112 <dudrhkdspt112@gmail.com>
2025-01-21 10:03:45 +0900

    리액트 훅 폼 사용

--- src/components/Authentication/LoginPage.jsx
+++ src/components/Authentication/LoginPage.jsx
@@ -1,32 +1,42 @@
 import React, { useRef, useState } from 'react'
 import './LoginPage.css';
+import { useForm } from 'react-hook-form';
 
 const LoginPage = () => {
     //const passwordRef = useRef(null);
 
-	const [user, setUser] = useState({
-		email: '',
-		password: '',
-	})
+	// const [user, setUser] = useState({
+	// 	email: '',
+	// 	password: '',
+	// })
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		console.log(user); //서버로 로그인
-		setUser({email:'', password:''});
-	}
+	// const handleSubmit = (e) => {
+	// 	e.preventDefault();
+	// 	console.log(user); //서버로 로그인
+	// 	setUser({email:'', password:''});
+	// }
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	  } = useForm();
+	
+	const submitData = (formData) => console.log(formData);
 
     return (
         <section className='align_center form_page'>
-			<form onSubmit={handleSubmit} className='authentication_form'>
+			<form onSubmit={handleSubmit(submitData)} className='authentication_form'>
 				<h2>로그인 폼</h2>
 				<div className='form_inputs'>
 					<div>
 						<label htmlFor='email'>Email</label>
 						<input
-							onChange={(e) => setUser({...user, email: e.target. value})}
-							value={user.email}
-							type='email'
-							id='email'
+							// onChange={(e) => setUser({...user, email: e.target. value})}
+							// value={user.email}
+							{...register('email')}
+							// type='email'
+							// id='email'
 							className='form_text_input'
 							placeholder='이메일 입력...'
 						/>
@@ -34,11 +44,12 @@ const LoginPage = () => {
 					<div>
 						<label htmlFor='password'>Password</label>
 						<input
-							onChange={(e) => setUser({ ...user, password: e.target.value })}
-							value={user.password}
-							type='password'
+							// onChange={(e) => setUser({ ...user, password: e.target.value })}
+							// value={user.password}
+							{...register('password')}
+							//type='password'
 							// ref={passwordRef}
-                            id='password'
+                            //id='password'
 							className='form_text_input'
 							placeholder='패스워드'
 						/>
